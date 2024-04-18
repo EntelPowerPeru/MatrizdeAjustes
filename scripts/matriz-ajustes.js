4811603f2c42c4430a7b1180c0b8912b20c5993c
EntelPowerPeru <leonidaaalarcon@gmail.com>
2024-04-18 10:49:35 -0500

    eeeeee

---
+++ scripts/matriz-ajustes.js
@@ -0,0 +1,14 @@
+const EVENTOS = {
+    init() {
+        this.irErrorDetectable();
+    },
+    irErrorDetectable() {
+        $("body").on("click", "#btn_error_detectable", (evento) => {
+            location = "/error-detectable.html"
+        });
+    }
+};
+
+(() => {
+    EVENTOS.init();
+})();

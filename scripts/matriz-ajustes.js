e18534cbf725ad9d312b0522210663cdeb07d45c
EntelPowerPeru <94659547+EntelPowerPeru@users.noreply.github.com>
2024-05-15 15:12:48 -0500

    Add files via upload

--- scripts/matriz-ajustes.js
+++ scripts/matriz-ajustes.js
@@ -1,10 +1,16 @@
 const EVENTOS = {
     init() {
         this.irErrorDetectable();
+        this.irErrorNoDetectable();
     },
     irErrorDetectable() {
         $("body").on("click", "#btn_error_detectable", (evento) => {
             location = "./error-detectable.html"
+        });
+    },
+    irErrorNoDetectable() {
+        $("body").on("click", "#btn_error_no_detectable", (evento) => {
+            location = "./error-no-detectable.html"
         });
     }
 };
